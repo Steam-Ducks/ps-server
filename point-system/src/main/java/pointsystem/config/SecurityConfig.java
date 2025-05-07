73b7e141a34a1c95da42d5080393e26c76c8c78f
Rafaella Cruz <rafaella.leticiaa@gmail.com>
2025-05-07 18:07:40 -0300

    refactor (PS-84): releasing access to the endpoint to make requests to the timerecords endpoint

--- point-system/src/main/java/pointsystem/config/SecurityConfig.java
+++ point-system/src/main/java/pointsystem/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package pointsystem.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,6 +38,7 @@ public class SecurityConfig {
                                 "/webjars/**",
                                 "/v3/api-docs.yaml"
                         ).permitAll() // Allow public access to auth endpoints
+                        .requestMatchers(HttpMethod.PUT, "/api/timerecords/**").permitAll()
                         .anyRequest().authenticated() // Require authentication for all other endpoints
                 )
                 .sessionManagement(session -> session
