af4dbdd55ec23520c556f10320c69b92d8597236
Isabelly S <164695350+61isabelly@users.noreply.github.com>
2025-05-20 17:28:51 -0300

    test (PS-51): configure H2 database

--- point-system/src/test/java/pointsystem/integration/PointSystemApplicationTests.java
+++ point-system/src/test/java/pointsystem/integration/PointSystemApplicationTests.java
@@ -1,8 +1,12 @@
 package pointsystem.integration;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@DataJpaTest
+@ActiveProfiles("test")
 @SpringBootTest
 class PointSystemApplicationTests {
 
